56bd2b0c9646f0b45c7ae794638a5713d138ec39
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-04-08 11:00:39 -0500

    Update controllers.py

--- shop_flow/controllers/controllers.py
+++ shop_flow/controllers/controllers.py
@@ -51,7 +51,7 @@ class DeliveryScheduleManager(http.Controller):
         group_number = int(kw['group'])
         group_size = int(kw['group_size'])
         is_done = False
-        data  = http.request.env['sale.order'].search([]).filtered(lambda x: x.commitment_date)#search([('commitment_date','!=',False)])
+        data  = http.request.env['sale.order'].search([('commitment_date','!=',False)])
         min = 0 + group_size*group_number
         max = min + group_size
         if max >= len(data):
